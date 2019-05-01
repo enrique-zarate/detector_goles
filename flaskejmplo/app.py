58c232d090cfeec1f90cb91dae967a4944acae76
Henry Raspberry <eszg.fury@gmail.com>
2019-05-01 16:27:23 -0400

    fix index

--- flaskejmplo/app.py
+++ flaskejmplo/app.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template
 import RPi.GPIO as gpio
 from time import sleep
+import time
+tiempo_anterior = 0
 
-foto1 = 4
-foto2 = 3
+foto1 = 27
+foto2 = 17
 foto3 = 6
 foto4 = 7
 
@@ -14,16 +16,34 @@ gpio.setup(foto3,gpio.IN)
 gpio.setup(foto4,gpio.IN)
 contador_goles = 0
 
-def sensor(llamada):
-	global contador_goles
-	contador_goles = contador_goles + 1 #este el contador
-	print(contador_goles)
-	
-if foto1 == True:
-	gpio.add_event_detect(foto1,gpio.RISING, callback=sensor, bouncetime=100)
-if foto2 == True:
-	gpio.add_event_detect(foto2,gpio.RISING, callback=sensor, bouncetime=100)
 
+def sensor(llamada):
+	global tiempo_anterior
+	tiempo_ahora = time.time()
+	diferencia_tiempos = tiempo_ahora - tiempo_anterior
+	if(diferencia_tiempos>3):
+		global contador_goles
+		contador_goles = contador_goles + 1 #este el contador
+		tiempo_anterior = tiempo_ahora
+		print("GOOOOOOL ",contador_goles)
+	else:
+		print("Debes esperar: ", 3-diferencia_tiempos)
+	
+
+# Si alguno de los sensores es FALSE, contar. Obviar el resto
+	#gpio.add_event_detect(foto1,gpio.RISING, callback=sensor, bouncetime=100)
+
+
+gpio.add_event_detect(foto1,gpio.FALLING, callback=sensor, bouncetime=100)
+#if foto1 == False
+gpio.add_event_detect(foto2,gpio.FALLING, callback=sensor, bouncetime=100)
+gpio.add_event_detect(foto3,gpio.FALLING, callback=sensor, bouncetime=100)
+gpio.add_event_detect(foto4,gpio.FALLING, callback=sensor, bouncetime=100)
+			
+			
+			
+
+			
 
 app=Flask(__name__)
 
@@ -47,5 +67,8 @@ if __name__=='__main__':
 			sensor()
 			sleep(2)
 	"""
+
+	
+		
 	app.run()
 		
